1a03dd2c092d5849cc9cef55fb0fb03cc69c0016
GAIDI AMJED <150559809+Gautier7799@users.noreply.github.com>
2026-09-14 17:53:21 +0100

    Add AppModel data class for application representation

---
+++ ​app/src/main/java/com/blend/launcher/data/model/AppModel.kt
@@ -0,0 +1,9 @@
+package com.blend.launcher.data.model
+
+import android.graphics.drawable.Drawable
+
+data class AppModel(
+    val label: String,
+    val packageName: String,
+    val icon: Drawable
+)
